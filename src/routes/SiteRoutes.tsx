e8087eb5ecb105bb21ff3e47acc9a8635c5576d2
fridizkool <fsorgtaylor@gmail.com>
2025-06-23 22:02:37 -0400

    change route

--- src/routes/SiteRoutes.tsx
+++ src/routes/SiteRoutes.tsx
@@ -3,8 +3,8 @@ import About from "../Pages/About/About";
 import MainPage from "../Pages/MainPage/MainPage";
 
 export const SiteRoutes: siteRouteType[] = [
-  { to: "./", component: <MainPage />, linkText: "Home" },
-  { to: "./about", component: <About />, linkText: "About me" },
+  { to: "/fredsite/", component: <MainPage />, linkText: "Home" },
+  { to: "/fredsite/about", component: <About />, linkText: "About me" },
 ];
 
 type siteRouteType = {
