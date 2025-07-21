67d809ef067fbd40ca31c893e74f4e2f224f9594
fridizkool <fsorgtaylor@gmail.com>
2025-07-21 17:36:39 -0400

    toys and blurb

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,14 +1,27 @@
+import { NavLink } from "react-router";
 import { MenuLink } from "../../components/NavBar/NavBar";
-import { SiteRoutes } from "../../routes/SiteRoutes";
+import { BASE, SiteRoutes } from "../../routes/SiteRoutes";
 
 export default function MainPage() {
   return (
-    <div className="grid grid-cols-12 grid-rows-12 gap-5">
+    <div className="grid grid-cols-12 gap-5">
       <div className="col-span-8 col-start-3 window-blurb introduction text-center">
         <h1>Welcome to my site!</h1>
         <img src="meat.gif" className="object-fill w-full pt-3"></img>
       </div>
-      <div className="col-span-8 col-start-3 row-span-1 window-blurb introduction text-center">
+      <div className="col-span-5 col-start-3 row-span-1 window-blurb introduction text-center">
+        <h2>
+          <NavLink to={BASE + "about"} className="text-white">
+            <u>Who am I?</u>
+          </NavLink>
+        </h2>
+        I am a developer with 1+ years of professional experience in the front
+        end developing the IRS payments systems. I am skilled in most ends of
+        software development and web development. From testing code to writing
+        untestable code, I have done it all. I always like to tinker with new
+        things.
+      </div>
+      <div className="col-span-3 row-span-1 window-blurb introduction text-center">
         {SiteRoutes.map((route) => {
           return <MenuLink to={route.to}>{route.linkText}</MenuLink>;
         })}
--- src/routes/SiteRoutes.tsx
+++ src/routes/SiteRoutes.tsx
@@ -1,10 +1,14 @@
 import type { ReactElement, ReactNode } from "react";
 import About from "../Pages/About/About";
 import MainPage from "../Pages/MainPage/MainPage";
+import Toys from "../Pages/Toys/Toys";
+
+export const BASE = "/fredsite/";
 
 export const SiteRoutes: siteRouteType[] = [
-  { to: "/fredsite/", component: <MainPage />, linkText: "Home" },
-  { to: "/fredsite/about", component: <About />, linkText: "About me" },
+  { to: BASE, component: <MainPage />, linkText: "Home" },
+  { to: BASE + "about", component: <About />, linkText: "About me" },
+  { to: BASE + "toys", component: <Toys />, linkText: "Toys" },
 ];
 
 type siteRouteType = {
