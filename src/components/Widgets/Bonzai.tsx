ec1a6a372d7d2c0938a2f146af28e63b427f05fd
fridizkool <fsorgtaylor@gmail.com>
2025-07-22 18:18:32 -0400

    fix bonzai conga

--- src/components/Widgets/Bonzai.tsx
+++ src/components/Widgets/Bonzai.tsx
@@ -1,37 +1,49 @@
-import React from "react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Bonzai() {
   const [enable, setEnable] = useState(false);
   const [clicked, setClicked] = useState(false);
   const congalala = useRef<HTMLImageElement>(null);
+
   useEffect(() => {
     congalala.current = document.createElement("img");
+    setClicked(false);
   }, []);
 
   return (
     //dumbest possible way of doing, for funsies
     <>
-      <a onClick={() => setEnable(!enable)}>Bonzai Conga</a>
+      <a
+        onClick={() => {
+          setEnable(!enable);
+          setClicked(false);
+        }}
+      >
+        Bonzai Conga
+      </a>
       {enable && (
-        <div className="fixed z-100">
-          <img
-            ref={congalala}
-            src="congalala.png"
-            draggable="false"
-            onMouseMove={(event) => {
-              if (congalala.current && clicked)
-                congalala.current.style =
-                  "position: fixed; left: " +
-                  (event.pageX - 500) +
-                  "px; top: " +
-                  (event.pageY - 500) +
-                  "px;";
-              console.log(event);
-            }}
-            onClick={() => setClicked(!clicked)}
-          />
-        </div>
+        <img
+          ref={congalala}
+          src="congalala.png"
+          draggable="false"
+          className="fixed z-10"
+          width="1000px"
+          height="1000px"
+          onMouseDown={(event) => {
+            event.preventDefault();
+            setClicked(true);
+          }}
+          onMouseUp={() => setClicked(false)}
+          onMouseLeave={() => setClicked(false)}
+          onMouseMove={(event) => {
+            if (!clicked || !congalala.current) return;
+
+            congalala.current.style.top =
+              event.clientY - congalala.current.height / 2 + "px";
+            congalala.current.style.left =
+              event.clientX - congalala.current.width / 2 + "px";
+          }}
+        />
       )}
     </>
   );
