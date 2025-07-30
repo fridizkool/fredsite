134f0b2c04b9099ced191e2a2ddd752940a7647b
fridizkool <fsorgtaylor@gmail.com>
2025-07-30 15:54:10 -0400

    fix conga, can talk now

--- src/components/Widgets/Bonzai/Bonzai.tsx
+++ src/components/Widgets/Bonzai/Bonzai.tsx
@@ -1,33 +1,39 @@
 import { useEffect, useRef, useState } from "react";
 import { Bonzais } from "./BonzaiText";
+import Draggable from "react-draggable";
 
 export default function Bonzai() {
   const [enable, setEnable] = useState(false);
-  const [clicked, setClicked] = useState(false);
   const [curText, setCurText] = useState("");
-  const congalala = useRef<HTMLImageElement>(null);
+  const congalala = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    congalala.current = document.createElement("img");
-    setClicked(false);
+    congalala.current = document.createElement("div");
   }, []);
 
-  useEffect(() => {
-    setClicked(false);
-  }, [enable]);
-
-  function search(event: React.MouseEvent<HTMLImageElement, MouseEvent>) {
-    event.preventDefault();
-    setClicked(false);
+  function search() {
     if (!congalala.current) return;
-    const x = congalala.current.x - 1;
-    const y = congalala.current.y - 1;
+    const coords = breakTransform(congalala.current.style.transform);
+    const x = coords.x + congalala.current.offsetLeft - 1;
+    const y = coords.y + congalala.current.offsetTop - 1;
     const ele = document.elementFromPoint(x, y);
     if (!ele) {
       setCurText("");
       return;
     }
+    console.log(ele);
     if (Bonzais[ele.id]) setCurText(Bonzais[ele.id]);
+  }
+
+  function breakTransform(transform: string) {
+    const location = transform.substring(10).split(")")[0];
+    const parts = location.split(", ");
+    const coords = {
+      x: Number.parseInt(parts[0].split("px")[0]),
+      y: Number.parseInt(parts[1].split("px")[0]),
+    };
+    console.log(location);
+    return coords;
   }
 
   return (
@@ -37,31 +43,23 @@ export default function Bonzai() {
         Bonzai Conga
       </a>
       {enable && (
-        <div className="fixed">
-          <span>{curText}</span>
-          <img
-            ref={congalala}
-            src="congalala.png"
-            draggable="false"
-            className="fixed z-10 cursor-grab select-none"
-            width={window.innerWidth / 10}
-            height={window.innerHeight / 10}
-            onMouseDown={(event) => {
-              event.preventDefault();
-              setClicked(true);
-            }}
-            onMouseUp={search}
-            onMouseLeave={search}
-            onMouseMove={(event) => {
-              if (!clicked || !congalala.current) return;
-
-              congalala.current.style.top =
-                event.clientY - congalala.current.height / 2 + "px";
-              congalala.current.style.left =
-                event.clientX - congalala.current.width / 2 + "px";
-            }}
-          />
-        </div>
+        <Draggable
+          nodeRef={congalala}
+          onStop={() => {
+            search();
+          }}
+        >
+          <div ref={congalala}>
+            <img
+              src="congalala.png"
+              draggable="false"
+              className="draggable-box"
+              width={window.innerWidth / 10}
+              height={window.innerHeight / 10}
+            />
+            <span>{curText}</span>
+          </div>
+        </Draggable>
       )}
     </>
   );
